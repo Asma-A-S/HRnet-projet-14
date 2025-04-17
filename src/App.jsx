79ed5f651556d1cb696d6adf260689504a12e752
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-04-17 12:32:33 +0200

    ajout router avec les 2 pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { CreateEmployee } from './pages/CreateEmployee'
+import { EmployeeList } from './pages/EmployeeList'
 function App() {
   return (
-    <h1>bonjour</h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<CreateEmployee/>}/>
+        <Route path='/EmployeeList' element={<EmployeeList/>}/>
+      </Routes>
+    </Router>
   )
 }
 
