432ad2b024971fd542056f4a4091edd18d0af1e7
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-05-03 17:25:49 +0200

    retirer initialformdata, et ajouter map pour les champs du formulaire

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,27 +1,8 @@
-import states from '../data/states'
 import { useState } from 'react'
 import Input from './Input'
 import Select from './Select'
-const Departments = [
-    'Sales',
-    'Marketing',
-    'Engineering',
-    'Human Resources',
-    'Legal',
-]
 
-export default function Form({ onSubmit }) {
-    const initialFormData = {
-        firstName: '',
-        lastName: '',
-        birthDate: '',
-        startDate: '',
-        street: '',
-        city: '',
-        state: states[0].abbreviation,
-        zipCode: '',
-        department: Departments[0],
-    }
+export default function Form({ fields, initialFormData, onSubmit }) {
     const [formData, setFormData] = useState(initialFormData)
 
     const handleChange = (key, inputValue) => {
@@ -32,86 +13,37 @@ export default function Form({ onSubmit }) {
         onSubmit(formData)
         setFormData(initialFormData)
     }
+
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <Input
-                    id="firstName"
-                    label="First Name"
-                    type="text"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                />
+                {fields.map((field) => {
+                    if (field.type === 'select') {
+                        return (
+                            <Select
+                                key={field.id}
+                                id={field.id}
+                                label={field.label}
+                                value={formData[field.id]}
+                                data={field.data}
+                                keyValue={field.keyValue}
+                                onChange={handleChange}
+                            />
+                        )
+                    }
 
-                <Input
-                    id="lastName"
-                    label="Last Name"
-                    type="text"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                />
-
-                <Input
-                    id="birthDate"
-                    type="date"
-                    label="Date of Birth"
-                    value={formData.birthDate}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    id="startDate"
-                    type="date"
-                    label="Start Date"
-                    value={formData.startDate}
-                    onChange={handleChange}
-                    required
-                />
-
-                <fieldset className="address">
-                    <legend>Address</legend>
-                    <Input
-                        id="street"
-                        type="text"
-                        label="Street"
-                        value={formData.street}
-                        onChange={handleChange}
-                        required
-                    />
-                    <Input
-                        id="city"
-                        type="text"
-                        label="City"
-                        value={formData.city}
-                        onChange={handleChange}
-                        required
-                    />
-                    <Select
-                        id="state"
-                        label="State"
-                        value={formData.state}
-                        data={states}
-                        keyValue="abbreviation"
-                        onChange={handleChange}
-                    />
-
-                    <Input
-                        id="zipCode"
-                        type="number"
-                        label="zip Code"
-                        value={formData.zipCode}
-                        onChange={handleChange}
-                        required
-                    />
-                </fieldset>
-                <Select
-                    id="department"
-                    label="Department"
-                    value={formData.department}
-                    data={Departments}
-                    onChange={handleChange}
-                />
-
+                    return (
+                        <Input
+                            key={field.id}
+                            id={field.id}
+                            label={field.label}
+                            type={field.type}
+                            value={formData[field.id]}
+                            required={field.required}
+                            onChange={handleChange}
+                        />
+                    )
+                })}
                 <button type="submit" className="button-submit">
                     Save
                 </button>
