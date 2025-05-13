f18c21ef4d5e552769baa6d1da1126d983ff4f30
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-05-13 12:53:26 +0200

    ajout doc et commentaires

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react'
 import Input from './Input'
 import Select from './Select'
 
+/**
+ * Composant Formulaire Générique
+ *
+ * @param {Array} fields - Liste des champs à afficher, avec leurs propriétés :
+ *    - id: identifiant unique du champ
+ *    - label: étiquette affichée
+ *    - type: "text", "email", "date", "select", etc.
+ *    - required: booléen pour champ obligatoire
+ *    - data: données pour les selects
+ *    - keyValue: pour les selects : { label: "nom", value: "id" }
+ *
+ * @param {Object} initialFormData - Valeurs par défaut des champs
+ * @param {Function} onSubmit - Fonction déclenchée à la soumission du formulaire
+ */
+
 export default function Form({ fields, initialFormData, onSubmit }) {
     const [formData, setFormData] = useState(initialFormData)
-
+    // Gère le changement de valeur dans les champs
     const handleChange = (key, inputValue) => {
         setFormData((rest) => ({ ...rest, [key]: inputValue }))
     }
+    //soumission et réinitilistion du formulaire
     const handleSubmit = (e) => {
         e.preventDefault()
         onSubmit(formData)
@@ -16,7 +32,7 @@ export default function Form({ fields, initialFormData, onSubmit }) {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} aria-label="Formulaire de saisie">
                 {fields.map((field) => {
                     if (field.type === 'select') {
                         return (
@@ -26,6 +42,7 @@ export default function Form({ fields, initialFormData, onSubmit }) {
                                 label={field.label}
                                 value={formData[field.id]}
                                 data={field.data}
+                                required={field.required}
                                 keyValue={field.keyValue}
                                 onChange={handleChange}
                             />
